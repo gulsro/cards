6cb05a18e938ce7335906a48398d41145c7a162d
gulser <glserozkan@gmail.com>
2023-10-15 21:44:26 +0200

    shuffle() is added

--- deck.go
+++ deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"os"
+	"math/rand"
+)
+
 // Create a new type of 'deck'
 // which is slice of strings
 type deck []string
@@ -27,4 +33,29 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	byteSlice, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	stringSlice := strings.Split(string(byteSlice), ",")
+	return deck(stringSlice)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -6,7 +6,11 @@ func main(){
 	cards := newDeck()
 
 	//cards.print()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	//hand, remainingCards := deal(cards, 5)
+	//hand.print()
+	//remainingCards.print()
+	// cards := newDeckFromFile("jello")
+	// cards.print()
+	cards.shuffle()
+	cards.print()
 }
